ac9bd92039e780b0f53eda98691ad5841a62f58f
Yoshihiro Imai <yoshihiro_imai@dwango.co.jp>
2017-06-18 22:16:59 +0900

    Exercise 3.1

--- 3/LeftistHeap.v
+++ 3/LeftistHeap.v
@@ -1,7 +1,8 @@
-Require Import List Program.
+Require Import List Program Arith.
 Open Scope list_scope.
 
 Require Import PFDS.common.Ordered.
+Require Import PFDS.common.Power.
 
 Declare Module Elem : Ordered.
   
@@ -23,8 +24,31 @@ Proof.
   now intros.
 Qed.
 
+Fixpoint size(h : heap) :=
+  match h with
+  | E => 0
+  | T _ _ a b => 1 + size(a) + size(b)
+  end.
+
+Inductive Leftist : heap -> Prop :=
+| LeftistE : Leftist E
+| LeftistT : forall a b r x,
+    rank b <= rank a -> Leftist a -> Leftist b -> Leftist (T r x a b).
+
 (** ** Exercise 3.1 *)
 
-
+Lemma ex3_1_aux : forall t, Leftist t -> 2 ^^ (rank t) <= size t + 1.
+Proof.
+  induction t as[| r x a IHa b IHb].
+  - reflexivity.
+  - rewrite rank_T. simpl size. intro HLeftist.
+    cutrewrite (S (size a + size b) + 1 = (size a + 1) + (size b + 1)); [| ring].
+    inversion HLeftist as [|a0 b0 r0 x0 Hrank HLa HLb]. subst. apply IHa in HLa. apply IHb in HLb.
+    apply (le_trans _ (pow 2 (rank a) + (size b + 1))); [|now apply plus_le_compat_r].
+    apply (le_trans _ (pow 2 (rank a) + pow 2 (rank b))); [|now apply plus_le_compat_l].
+    rewrite pow_plus. simpl (pow 2 1).
+    cutrewrite (2 * pow 2 (rank b) = pow 2 (rank b) + pow 2 (rank b)); [|ring].
+    apply (plus_le_compat_r). now apply pow_le; [now auto with arith|].
+Qed.
 
 
