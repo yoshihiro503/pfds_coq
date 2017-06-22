0860c9348978b47a42946e0ee175fed21fb2eba6
Yoshihiro Imai <yoshihiro_imai@dwango.co.jp>
2017-06-22 16:28:57 +0900

    p.28のmerge関数の実装をCoqで。

--- 3/LeftistHeap.v
+++ 3/LeftistHeap.v
@@ -16,10 +16,10 @@ Fixpoint right_spine(h : heap) :=
   | T r x left_ right_ => x :: right_spine right_
   end.
 
-Definition rank(h : heap) := List.length (right_spine h).
+Definition calc_rank(h : heap) := List.length (right_spine h).
 
 Lemma rank_T : forall r x left_ right_,
-    rank(T r x left_ right_) = 1 + rank(right_).
+    calc_rank(T r x left_ right_) = 1 + calc_rank(right_).
 Proof.
   now intros.
 Qed.
@@ -33,22 +33,61 @@ Fixpoint size(h : heap) :=
 Inductive Leftist : heap -> Prop :=
 | LeftistE : Leftist E
 | LeftistT : forall a b r x,
-    rank b <= rank a -> Leftist a -> Leftist b -> Leftist (T r x a b).
+    calc_rank b <= calc_rank a -> Leftist a -> Leftist b -> Leftist (T r x a b).
 
 (** ** Exercise 3.1 *)
 
-Lemma ex3_1_aux : forall t, Leftist t -> 2 ^^ (rank t) <= size t + 1.
+Lemma ex3_1_aux : forall t, Leftist t -> 2 ^^ (calc_rank t) <= size t + 1.
 Proof.
   induction t as[| r x a IHa b IHb].
   - reflexivity.
   - rewrite rank_T. simpl size. intro HLeftist.
     cutrewrite (S (size a + size b) + 1 = (size a + 1) + (size b + 1)); [| ring].
     inversion HLeftist as [|a0 b0 r0 x0 Hrank HLa HLb]. subst. apply IHa in HLa. apply IHb in HLb.
-    apply (le_trans _ (pow 2 (rank a) + (size b + 1))); [|now apply plus_le_compat_r].
-    apply (le_trans _ (pow 2 (rank a) + pow 2 (rank b))); [|now apply plus_le_compat_l].
+    apply (le_trans _ (pow 2 (calc_rank a) + (size b + 1))); [|now apply plus_le_compat_r].
+    apply (le_trans _ (pow 2 (calc_rank a) + pow 2 (calc_rank b))); [|now apply plus_le_compat_l].
     rewrite pow_plus. simpl (pow 2 1).
-    cutrewrite (2 * pow 2 (rank b) = pow 2 (rank b) + pow 2 (rank b)); [|ring].
+    cutrewrite (2 * pow 2 (calc_rank b) = pow 2 (calc_rank b) + pow 2 (calc_rank b)); [|ring].
     apply (plus_le_compat_r). now apply pow_le; [now auto with arith|].
 Qed.
 
+(**
+ * ** merge関数
+ * > どの右スパインの長さもたかだか対数のオーダーであるから、[merge]は O(log n) 時間で実行される。 (p.28)
+ *)
+
+(**
+ * *** merge関数の実装
+ *)
+
+Definition rank h :=
+  match h with
+  | E => 0
+  | T r _ _ _ => r
+  end.
+
+Definition makeT x a b :=
+  if le_lt_dec (rank a) (rank b) then T (1 + rank b) x a b
+  else T (1 + rank a) x b a.
+
+Require Import Recdef.
+
+Definition pair_size {A B : Set} size1 size2 (pair: A*B) :=
+  let '(x, y) := pair in
+  size1 x + size2 y.
+
+Function merge h1_h2 {measure (pair_size size size) h1_h2} :=
+  match (h1_h2) with
+  | (E, h2) => h2
+  | (h1, E) => h1
+  | (T _ x a1 b1 as h1, T _ y a2 b2 as h2) =>
+    if Elem.leq_bool x y then
+      makeT x a1 (merge (b1, h2))
+    else
+      makeT y a2 (merge (h1, b2))
+  end.
+Proof. (* 停止性の証明 *)
+  - simpl. now auto with arith.
+  - simpl. now auto with arith.
+Defined.
 
