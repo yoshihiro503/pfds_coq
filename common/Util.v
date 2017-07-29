ef7c5d0943d1e2ce2ed14d9a12d8112d1039d05d
Yoshihiro Imai <yoshihiro_imai@dwango.co.jp>
2017-07-29 21:39:00 +0900

    fold_rightに関する補題をいくつか導入。TODO: いくつかは未証明

--- common/Util.v
+++ common/Util.v
@@ -1,8 +1,9 @@
 Require Import Program List String.
 Require Import PFDS.common.Result.
 Open Scope string_scope.
+Open Scope list_scope.
 
-  Definition pair_size {A B : Set} size1 size2 (pair: A*B) :=
+Definition pair_size {A B : Set} size1 size2 (pair: A*B) :=
   let '(x, y) := pair in
   size1 x + size2 y.
 
@@ -23,3 +24,32 @@ Definition reduce_left {A: Type} (f: A -> A -> A) (xs: list A) : Result A :=
   end.
 
 Definition reduce {A: Type} := @reduce_right A.
+
+Lemma fold_right_tail : forall (A B: Type) (f: A -> B -> B) xs x b0,
+  List.fold_right f b0 (xs ++ [x]) = List.fold_right f (f x b0) xs.
+Proof.
+Admitted.
+
+Lemma fold_right_rev : forall (A: Type) (f: A -> A -> A) xs x0,
+  (forall x y z, f x (f y z) = f (f x y) z) ->
+  (forall x y, f x y = f y x) ->
+  List.fold_right f x0 xs = List.fold_right f x0 (List.rev xs).
+Proof.
+Admitted.
+
+Lemma fold_right_cons_tail : forall (A:Type) (f: A -> A -> A) x xs y ys,
+  (forall x y z, f x (f y z) = f (f x y) z) ->
+  (forall x y, f x y = f y x) ->
+  x :: xs = ys ++ [y] ->
+  fold_right f x xs = fold_right f y ys.
+Proof.
+ intros A f x xs y ys Hassoc Hcomm Heq. destruct xs as [|x1 xs'].
+ - destruct ys; [|destruct ys; discriminate Heq]. simpl in Heq. now inversion Heq.
+ - destruct ys as [|y1 ys']; [discriminate|].
+   inversion Heq. rewrite H1.
+   rewrite fold_right_tail. rewrite Hcomm.
+   rewrite fold_right_rev at 1; [|assumption|assumption].
+   rewrite <- fold_right_tail.
+   rewrite fold_right_rev at 1; [|assumption|assumption].
+   rewrite rev_unit. now rewrite rev_involutive.
+Qed.
