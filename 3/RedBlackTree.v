3e6931f63b116d54ab378a41e409fc2a14781452
Yoshihiro Imai <yoshihiro_imai@dwango.co.jp>
2017-08-01 23:48:01 +0900

    balance関数がBalanced性質を保存することを証明

--- 3/RedBlackTree.v
+++ 3/RedBlackTree.v
@@ -34,6 +34,7 @@ Inductive IsRootBlack : tree -> Prop :=
  - treeのElem.Tの順序については何も言っていない
  *)
 
+(*
 Inductive RedBlackWithLength : nat -> tree -> Prop :=
 | RBEmpty : RedBlackWithLength 0 E
 | RBRed : forall x a b n,
@@ -44,6 +45,18 @@ Inductive RedBlackWithLength : nat -> tree -> Prop :=
     RedBlackWithLength n a -> RedBlackWithLength n b ->
       RedBlackWithLength (1+n) (T 黒 a x b)
 .
+*)
+
+(* 赤黒木の条件1: すべての経路において黒の数が同一である([n]個) *)
+Inductive BalancedWithLength : nat -> tree -> Prop :=
+| BE : BalancedWithLength 0 E
+| BRed : forall n x a b, BalancedWithLength n a -> BalancedWithLength n b -> BalancedWithLength n (T 赤 a x b)
+| BBlack : forall n x a b, BalancedWithLength n a -> BalancedWithLength n b -> BalancedWithLength (1+n) (T 黒 a x b)
+.
+
+(* 赤黒木の条件2: すべての経路に置いて赤が2連続で現れない *)
+(* TODO *)
+Inductive WellColored : tree -> Prop :=.
 
 (**
  ** 演習 3.8
@@ -91,3 +104,49 @@ Definition insert (x: Elem.T) (s: tree) : tree :=
   | T _ a y b => T 黒 a y b
   | E => E (* 本来ここには来得ないパターン *)
   end.
+
+(*Functional Scheme ins_ind := Induction for ins Sort Prop.*)
+
+Lemma balance_aux : forall (P : (color*tree*Elem.T*tree) -> tree -> Prop),
+  (forall col t1 e t2 a b c d x y z,
+      col = 黒 -> t1 = T 赤 (T 赤 a x b) y c -> e = z -> t2 = d -> P (col,t1,e,t2) (T 赤 (T 黒 a x b) y (T 黒 c z d))) ->
+  (forall col t1 e t2 a b c d x y z,
+      col = 黒 -> t1 = T 赤 a x (T 赤 b y c) -> e = z -> t2 = d -> P (col,t1,e,t2) (T 赤 (T 黒 a x b) y (T 黒 c z d))) ->
+  (forall col t1 e t2 a b c d x y z,
+      col = 黒 -> t1 = a -> e = x -> t2 = T 赤 (T 赤 b y c) z d -> P (col,t1,e,t2) (T 赤 (T 黒 a x b) y (T 黒 c z d))) ->
+  (forall col t1 e t2 a b c d x y z,
+      col = 黒 -> t1 = a -> e = x -> t2 = T 赤 b y (T 赤 c z d) -> P (col,t1,e,t2) (T 赤 (T 黒 a x b) y (T 黒 c z d))) ->
+  (forall col t1 e t2, P (col,t1,e,t2) (T col t1 e t2)) ->
+  forall col t1 e t2, P (col,t1,e,t2) (balance col t1 e t2).
+Proof.
+ intros P H1 H2 H3 H4 Hother col t1 e t2.
+ set (Hother col t1 e t2).
+ set (H1 col t1 e t2) as H1'.
+ set (H2 col t1 e t2) as H2'.
+ set (H3 col t1 e t2) as H3'.
+ set (H4 col t1 e t2) as H4'.
+ destruct col; [now auto|].
+ (destruct t1 as [|c1 t11 x1 t12]; [|destruct c1;
+                                     (destruct t11 as [|c11 t11_1 x11 t11_2]; [|destruct c11]);
+                                     (destruct t12 as [|c12 t12_1 x12 t12_2]; [|destruct c12])
+                                   ]);
+   (destruct t2 as [|c2 t21 x2 t22]; [|destruct c2;
+                                       (destruct t21 as [|c21 t21_1 x21 t21_2]; [|destruct c21]);
+                                       (destruct t22 as [|c22 t22_1 x22 t22_2]; [|destruct c22])
+  ]); unfold balance; try (now auto).
+Qed.
+
+Lemma balance_Balanced : forall n color elem t1 t2,
+  BalancedWithLength n (T color t1 elem t2) -> BalancedWithLength n (balance color t1 elem t2)
+.
+Proof.
+  intros n col e t1 t2.
+  apply (balance_aux (fun '(col,t1,e,t2) t =>
+                    BalancedWithLength n (T col t1 e t2)->BalancedWithLength n t));
+    intros; subst.
+  - inversion H3. inversion H2. inversion H9.  subst. constructor; now constructor.
+  - inversion H3. inversion H2. inversion H11. subst. constructor; now constructor.
+  - inversion H3. inversion H5. inversion H9.  subst. constructor; now constructor.
+  - inversion H3. inversion H5. inversion H11. subst. constructor; now constructor.
+  - assumption.
+Qed.
